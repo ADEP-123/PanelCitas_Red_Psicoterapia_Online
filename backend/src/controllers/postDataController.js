28602e413a173791ae52c239708718b03970fff3
ADEP-123 <andep210399@gmail.com>
2023-07-22 12:13:28 -0500

    feat: :sparkles: Crud Paciente: Post
    Se crea el endpoint post para el paciente, se arregla lo de validaar si es menor de edad

--- backend/src/controllers/postDataController.js
+++ backend/src/controllers/postDataController.js
@@ -1,3 +1,4 @@
+import { getAcudienteService } from "../services/getServices.js";
 import { postTipoCitaService, postTipoDocumentoService, postEstadoCitaService, postPacienteService, postAcudienteService } from "../services/postServices.js";
 
 const postTipoCitaController = async (req, res, next) => {
@@ -43,9 +44,21 @@ const postAcudienteController = async (req, res, next) => {
 const postPacienteController = async (req, res, next) => {
     try {
         const { id, tipoDocumento, nombre, genero, fechaNacimiento, acudiente, telefPersonal, telefHogar, email } = req.body
-        const edad = Math.floor((new Date() - fechaNacimiento) / (1000 * 60 * 60 * 24 * 365.25));
-        const result = await postPacienteService(id, tipoDocumento, nombre, genero, fechaNacimiento, acudiente, telefPersonal, telefHogar, email);
-        res.status(200).json({ message: `Nuevo paciente \'${nombre}\' creado con exito`, result })
+        const edad = Math.floor((new Date() - new Date(fechaNacimiento)) / (1000 * 60 * 60 * 24 * 365.25));
+        let result;
+        if (edad < 18) {
+            result = await getAcudienteService(acudiente);
+            if (result.length == 0 || result.length > 1) {
+                res.status(500).json({ message: "El paciente que intenta crear es menor y no esta enviando informacion del acudiente o el acudiente enviado no se encuentra registrado, primero registre el acudiente" });
+            } else {
+                result = await postPacienteService(id, tipoDocumento, nombre, genero, fechaNacimiento, acudiente, telefPersonal, telefHogar, email);
+                res.status(200).json({ message: `Nuevo paciente \'${nombre}\' creado con exito`, result })
+            }
+        } else {
+            result = await postPacienteService(id, tipoDocumento, nombre, genero, fechaNacimiento, acudiente, telefPersonal, telefHogar, email);
+            res.status(200).json({ message: `Nuevo paciente \'${nombre}\' creado con exito`, result })
+        }
+
     } catch (error) {
         res.status(500).json(error);
     }
